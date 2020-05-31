3417bc3c9cee1d483c3ba12f28112e98181f1ed4
Chris Little <chrisdlittle@aol.com>
2020-05-30 21:19:10 -0400

    created burgers_controller.js

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const burger = require("../models/burger.js");
+
+const router = express.Router();
+
+router.get("/", function (req, res) {
+  burger.all(function (data) {
+    var burgerObj = {
+      burgers: data,
+    };
+    console.log(burgerObj);
+    res.render("index", burgerObj);
+  });
+});
+
+router.post("/api/burgers", function (req, res) {
+  burger.create(["burger_name"], [req.body.burger_name], function (result) {
+    res.json({ id: result.insertId });
+  });
+});
+
+router.put("/api/burgers/:id", function (req, res) {
+  var condition = "id = " + req.params.id;
+  burger.update(
+    {
+      devoured: req.body.devoured,
+    },
+    condition,
+    function (result) {
+      if (result.changedRows == 0) {
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    }
+  );
+});
+
+module.exports = router;
