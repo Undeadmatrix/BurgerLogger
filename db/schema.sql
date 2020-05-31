4aa8546fac246cb2b4b960528b995fb856834a79
Chris Little <chrisdlittle@aol.com>
2020-05-30 21:00:44 -0400

    wrote schema.sql and seeds.sql

--- db/schema.sql
+++ db/schema.sql
@@ -4,5 +4,8 @@ CREATE database burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers (
-    
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(255) NOT NULL,
+    devoured BOOLEAN DEFAULT false,
+    PRIMARY KEY (id)
 );
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("Ultimate");
+INSERT INTO burgers (burger_name) VALUES ("Dave's Favorite");
+INSERT INTO burgers (burger_name) VALUES ("Jacked-N-Stacked");
