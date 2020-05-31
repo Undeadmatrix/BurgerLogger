a537e008e843ece16ff064291bba79ef89a54972
Chris Little <chrisdlittle@aol.com>
2020-05-30 22:53:49 -0400

    attempt 2 bug fix

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,9 @@
 const mysql = require("mysql");
 var connection;
 
-/* if (process.env.JAWSDB_URL) {
+if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
-} */ /* else { */
+} else {
   connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -11,6 +11,7 @@ var connection;
     password: "test123",
     database: "burgers_db",
   });
+}
 
 connection.connect(function (err) {
   if (err) {
